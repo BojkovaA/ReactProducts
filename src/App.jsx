1aac8824ddc0da0c4d34bcda72268b81ab10b9e7
BojkovaA <108904427+BojkovaA@users.noreply.github.com>
2025-01-25 19:19:39 +0100

    Some Exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css'
 import { ToastContainer } from 'react-toastify';
 import ProductService from '../src/services/productService.js'
@@ -8,6 +8,13 @@ import axios from 'axios';
 axios.defaults.baseURL="https://dummyjson.com"
 
 function App() {
+
+  const [name, setName] = useState('Tihomir')
+  // console.log(name)
+
+  function changeName(){
+    setName('Angela')
+  }
 
   useEffect(()=>{
     ProductService.getSingleProduct()
@@ -20,7 +27,9 @@ function App() {
       <div className='container mx-auto flex flex-col items-center justify-center'>
         <h1 className='text-[60px] text-green-600 font-extrabold my-[30px]'>TODO APP</h1>
       </div>
-      <ToastContainer></ToastContainer>
+      <h1>{name}</h1>
+      <button onClick={changeName}>Change name</button>
+      <ToastContainer/>
     </>
   )
 }
